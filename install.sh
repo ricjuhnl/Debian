c8d9413b0c11174a012d48c9df0269984ea13793
Ricardo <ricardo.juhasz@maastrichtuniversity.nl>
2023-10-16 13:10:12 +0200

    Remove docker steps

--- install.sh
+++ install.sh
@@ -79,25 +79,6 @@ cd /home/$username/
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 curl -L git.io/antigen > antigen.zsh
 
-# Add Docker's official GPG key:
-sudo apt install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-
-# Install docker components
-sudo nala install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-# Add user to docker group
-sudo usermod -aG docker $username
-
 # Activate ZSH
 sudo usermod --shell /bin/zsh $username
 
