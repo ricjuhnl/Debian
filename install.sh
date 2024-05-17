3ad15fbc9d640ce6783796ccdf14345e3fe0c366
Ricardo <ricardo.juhasz@maastrichtuniversity.nl>
2024-05-17 14:26:52 +0000

    Updates

--- install.sh
+++ install.sh
@@ -19,24 +19,24 @@ mkdir -p /home/$username/.local/share/fonts
 sudo chown -R $username:$username /home/$username
 
 # Installing Essential Programs
-sudo apt install zsh curl picom keychain x11-xserver-utils unzip wget build-essential cifs-utils openconnect network-manager-openconnect network-manager-openconnect-gnome network-manager-openvpn network-manager-vpnc -y
+sudo apt install zsh curl picom keychain x11-xserver-utils unzip wget build-essential cifs-utils openconnect network-manager-openconnect network-manager-openconnect-gnome -y
 # Installing Other less important Programs
-sudo apt install nfs-common sshfs neofetch kitty micro fonts-noto-color-emoji pip appstream-util papirus-icon-theme nitrogen -y
+sudo apt install nfs-common sshfs neofetch kitty micro fonts-noto-color-emoji pip appstream-util papirus-icon-theme -y
 
 # Installing fonts
 cd $builddir
 sudo apt install fonts-font-awesome -y
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip
 unzip FiraCode.zip -d /home/$username/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.zip
-unzip Meslo.zip -d /home/$username/.local/share/fonts
+wget https://github.com/microsoft/cascadia-code/releases/download/v2404.23/CascadiaCode-2404.23.zip
+unzip CascadiaCode-2404.23.zip -d /home/$username/.local/share/fonts
 sudo chown $username:$username /home/$username/.local/share/fonts/*
 
 # Reloading Font
 fc-cache -vf
 
 # Removing zip Files
-rm ./FiraCode.zip ./Meslo.zip
+rm ./FiraCode.zip ./CascadiaCode-2404.23.zip
 
 # Install Nordzy cursor
 git clone https://github.com/alvatip/Nordzy-cursors
